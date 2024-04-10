48acd569df4880a84763022d6fac87c2f7d87f94
krishnakorukanti <crishnakorukanti@gmail.com>
2024-04-10 12:29:20 +0530

    Updated Contact Info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import {Github, Linkedin, Mail, Twitter} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -7,22 +7,28 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/crishna_k",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@crishna_k",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:hello@crishna.in",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "hello@crishna.in",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/krishnakorukanti",
 		label: "Github",
-		handle: "chronark",
+		handle: "krishnakorukanti",
 	},
+	{
+		icon: <Linkedin size={20}/>,
+		href: "https://www.linkedin.com/in/krishnakorukanti/",
+		label: "LinkedIn",
+		handle: "krishnakorukanti"
+	}
 ];
 
 export default function Example() {
